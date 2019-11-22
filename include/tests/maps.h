d21c6b5383b1378cf333751107acefc203b9f5d3
Renard <ludovic.fernandez@epitech.eu>
2019-11-22 12:45:34 +0100

    test(BSQ) Add hard coded maps for unit tests

---
+++ include/tests/maps.h
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2019
+** BSQ
+** File description:
+** BSQ maps for unit tests
+*/
+
+#ifndef MAPS_H
+#define MAPS_H
+
+#include "fox_define.h"
+
+extern const char UTMAP_10_10_D3[113];
+extern const char UTMAP_10_10_D3_INVALIDL1_A[116];
+extern const char UTMAP_10_10_D3_INVALIDL1_B[116];
+extern const char UTMAP_10_10_D3_INVALIDLC[113];
+extern const char UTMAP_10_10_D3_INVALIDLC[113];
+
+#define ASSERT_FWRITE_MAP(mapname, fp)                                  \
+    cr_assert_eq(                                                       \
+        fwrite((mapname), sizeof(*(mapname)), sizeof((mapname)), (fp)), \
+        sizeof((mapname)) / sizeof((*mapname)))
+
+#endif /* !MAPS_H */
---
+++ tests/test_maps_def.c
@@ -0,0 +1,75 @@
+/*
+** EPITECH PROJECT, 2019
+** <project name> unit tests
+** File description:
+** test_map_def.c -- No description
+*/
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include "tests/maps.h"
+
+const char UTMAP_10_10_D3[113] =
+    "10\n"
+    "...o..o...\n"
+    "o.........\n"
+    "......o.o.\n"
+    ".....o.o.o\n"
+    "..o.o....o\n"
+    "..........\n"
+    "......oo.o\n"
+    ".......o..\n"
+    "o.o.o..o..\n"
+    "....o.....\n";
+
+const char UTMAP_10_10_D3_INVALIDL1_A[116] =
+    "   10\n"
+    "...o..o...\n"
+    "o.........\n"
+    "......o.o.\n"
+    ".....o.o.o\n"
+    "..o.o....o\n"
+    "..........\n"
+    "......oo.o\n"
+    ".......o..\n"
+    "o.o.o..o..\n"
+    "....o.....\n";
+
+const char UTMAP_10_10_D3_INVALIDL1_B[116] =
+    "10   \n"
+    "...o..o...\n"
+    "o.........\n"
+    "......o.o.\n"
+    ".....o.o.o\n"
+    "..o.o....o\n"
+    "..........\n"
+    "......oo.o\n"
+    ".......o..\n"
+    "o.o.o..o..\n"
+    "....o.....\n";
+
+const char UTMAP_10_10_D3_INVALIDLL[113] =
+    "10\n"
+    "...o..o...\n"
+    "o.........\n"
+    "......o.o.\n"
+    ".....o.o.o\n"
+    "..o.o..\n"
+    "..........\n"
+    "......oo.o\n"
+    ".......o..\n"
+    "o.o.o..o..\n"
+    "....o.....\n";
+
+const char UTMAP_10_10_D3_INVALIDLC[113] =
+    "10\n"
+    "...o..o...\n"
+    "o.........\n"
+    "......o.o.\n"
+    ".....o.o.o\n"
+    "..o.o..\n"
+    "..........\n"
+    "......oo.o\n"
+    ".......o..\n"
+    "o.o.o..o..\n"
+    "....o.....\n";
