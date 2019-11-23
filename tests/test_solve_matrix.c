0c08cf37cd1b91688ea52016c919fb15c272efc8
Renard <ludovic.fernandez@epitech.eu>
2019-11-23 18:01:59 +0100

    test(BSQ) Fix solve_matrix tests

--- tests/test_solve_matrix.c
+++ tests/test_solve_matrix.c
@@ -15,53 +15,58 @@
 #include "bsq.h"
 
 __Anonnull
-static bool test_solve_matrix(mapdims_t *md, str2c_t buff)
+static bool test_matrix(mapdims_t *md, str_t mapbuff)
 {
-    uint (*mtxp)[md->y][md->x] = NULL;
-    bool r = alloc_matrix(md, &mtxp);
+    uint(*mtxp)[md->y][md->x] = NULL;
+    bool b = false;
 
-    if (r)
+    if (alloc_matrix(md, &mtxp)
+        || solve_matrix(md, *mtxp, mapbuff + fox_unumsize(md->y) + 1))
         return true;
-    r = solve_matrix(md, *mtxp, buff + fox_unumsize(md->y) + 1);
-    for (uint y = 0; !r && y < md->y; y += 1)
-        for (uint x = 0; !r && x < md->x; x += 1)
-            r = (UTMTX_6_6_D25[y][x] == (*mtxp)[y][x]);
-    for (uint y = 0; !r && y < md->y; y += 1)
-        for (uint x = 0; !r && x < md->x; x += 1)
-            r = (UTMTX_6_6_D25[y][x] == (*mtxp)[y][x]);
-    return r;
+    for (uint y = 0; !b && y < md->y; y += 1)
+        for (uint x = 0; !b && x < md->x; x += 1)
+            b = (UTMTX_6_6_D25[y][x] != (*mtxp)[y][x]);
+    return b;
+}
+
+__Anonnull
+static bool write_map_file(str2c_t path, const char map[44])
+{
+    FILE *fp = fopen(path, "w+");
+
+    if (fp == NULL
+        || fwrite(map, sizeof(*map), strlen(map), fp) != strlen(map)
+        || fclose(fp))
+        return true;
+    return false;
 }
 
 Test(solve_matrix, regular_usage)
 {
     str2c_t path = "UTMAP_6_6_D25-solvetest.tmp";
-    FILE *fp = fopen(path, "w+");
     mapdims_t md = {.x = 0, .y = 0};
     mapstat_t ms = {-1, {0}};
     str_t mapbuff = NULL;
 
-    cr_assert_not_null(fp);
-    ASSERT_FWRITE_MAP(UTMAP_6_6_D25, fp);
-    cr_assert_not(open_map(path, &ms)
-        || SUCCEED_IF_DIFF(read_map(&mapbuff, &ms), -1)
-        || get_dimensions(&md, &ms, mapbuff));
-    md.x += 1;
-    cr_assert_not(test_solve_matrix(&md, mapbuff));
+    cr_assert_not(write_map_file(path, UTMAP_6_6_D25));
+    cr_assert_not(open_map(path, &ms));
+    cr_assert(read_map(&mapbuff, &ms));
+    cr_assert_not(get_dimensions(&md, &ms, mapbuff));
+    cr_assert_not(write_map_file(path, UTMAP_6_6_D25));
+    cr_assert_not(test_matrix(&md, mapbuff));
 }
 
 Test(solve_matrix, invalid_char)
 {
     str2c_t path = "UTMAP_6_6_D25-invalid_char.tmp";
-    FILE *fp = fopen(path, "w+");
     mapdims_t md = {.x = 0, .y = 0};
     mapstat_t ms = {-1, {0}};
     str_t mapbuff = NULL;
 
-    cr_assert_not_null(fp);
-    ASSERT_FWRITE_MAP(UTMAP_6_6_INVALIDCHAR, fp);
-    cr_assert_not(open_map(path, &ms)
-        || SUCCEED_IF_DIFF(read_map(&mapbuff, &ms), -1)
-        || get_dimensions(&md, &ms, mapbuff));
-    md.x += 1;
-    cr_assert_not(test_solve_matrix(&md, mapbuff));
+    cr_assert_not(write_map_file(path, UTMAP_6_6_INVALIDCHAR));
+    cr_assert_not(open_map(path, &ms));
+    cr_assert(read_map(&mapbuff, &ms));
+    cr_assert_not(get_dimensions(&md, &ms, mapbuff));
+    cr_assert_not(write_map_file(path, UTMAP_6_6_D25));
+    cr_assert(test_matrix(&md, mapbuff));
 }
